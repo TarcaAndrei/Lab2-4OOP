98cab7858574dac2ff4737cdc9da450bb4d8f2e3
Valentin Serban <valentinserban@Valentins-MacBook-Pro.local>
2023-03-24 19:33:23 +0200

    mesaj

---
+++ Undo/undo.c
@@ -0,0 +1,5 @@
+//
+// Created by Valentin Serban on 24.03.2023.
+//
+
+#include "undo.h"
---
+++ Undo/undo.h
@@ -0,0 +1,8 @@
+//
+// Created by Valentin Serban on 24.03.2023.
+//
+
+#ifndef LAB2_4_UNDO_H
+#define LAB2_4_UNDO_H
+
+#endif //LAB2_4_UNDO_H
